52a1cc1460a70b211aafba3f485ddabb824c87b1
piad <piotr.andrychiewicz@mobica.com>
2018-06-09 15:02:02 +0200

    Added single food object

---
+++ Assets/Script/FoodLogic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodLogic : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D co)
+    {
+        if ("player".Equals(co.name)) {
+            Destroy(gameObject);
+        }
+    }
+}
