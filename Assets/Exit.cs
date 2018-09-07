0f1ec0994bd241d459dd58e515f34dff1c7cb7dd
Andrzej Pawlak <andrzej_pawlak90@o2.pl>
2018-09-07 22:55:22 +0200

    In App Purchase

---
+++ Assets/Exit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+
+	
+	public void ShowScen() {
+        SceneManager.LoadScene("walls");
+      
+    }
+	
+
+}
