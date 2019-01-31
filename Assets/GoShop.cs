b7c49c5eb88b858135902c07c0aa113e7179c9f1
andrzejpawlak123 <andrzej_pawlak90@o2.pl>
2019-01-31 23:30:06 +0100

    InAppPurchase logic

---
+++ Assets/GoShop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoShop : MonoBehaviour {
+
+    // Use this for initialization
+    public void ShowShop()
+    {
+        SceneManager.LoadScene("menu");
+
+    }
+}
---
+++ Assets/IAPModal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAPModal : IAPMenager {
+
+
+    public void Buy_1()
+    {
+        IAPMenager.Instance.Buy_Product_food();
+    }
+    public void Buy_2()
+    {
+        IAPMenager.Instance.Buy_Product_life();
+    }
+    public void Buy_NO_Ads()
+    {
+        IAPMenager.Instance.Buy_Product_noads();
+    }
+}
